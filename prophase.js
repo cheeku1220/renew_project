e50e051e30e58f7f9fd6336e959ec221c970b218
Pranav Sharma <80578881+cheeku1220@users.noreply.github.com>
2024-02-22 10:55:44 +0530

    Add files via upload

---
+++ prophase.js
@@ -0,0 +1,12 @@
+var obj1 = document.getElementById("fol");
+var obj2 = document.getElementById("chat");
+var obj3 = document.getElementById("vc");
+var obj4 = document.getElementById("ver");
+function showme() {
+    let d = document.getElementById("showps");
+    d.style.display = "block";
+}
+function hideme() {
+    let d = document.getElementById("showps");
+    d.style.display = "none";
+}
