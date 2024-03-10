37aa572db2fb69f02211681a29076082f2137d0b
Pranav Sharma <80578881+cheeku1220@users.noreply.github.com>
2024-03-10 19:35:35 +0530

    Update renew.js

--- renew.js
+++ renew.js
@@ -1,17 +1,17 @@
 function login() {
-    let d = document.getElementById("login");
-    d.style.display = "block";
-    // d.style.zIndex = "1";
-    d.style.backgroundColor = "rgba(0, 0, 0, 0.4)";
+
+    let s = document.getElementById("login");
+    s.style.display = "block";
     var span = document.getElementsByClassName("close")[0];
     span.onclick = function () {
-        d.style.display = "none";
+        s.style.display = "none";
     }
-    window.onclick = function (event) {
-        if (event.target == d) {
-            d.style.display = "none";
-        }
-    }
+
+    // window.onclick = function (event) {
+        // if (event.target == d) {
+            // d.style.display = "none";
+        // }
+    // }
 }
 function lookup(id) {
     var p = document.getElementsByTagName("pg");
@@ -30,9 +30,70 @@ function signup() {
     span.onclick = function () {
         d.style.display = "none";
     }
-    window.onclick = function (event) {
-        if (event.target == d) {
-            d.style.display = "none";
+    // window.onclick = function (event) {
+        // if (event.target == d) {
+            // d.style.display = "none";
+        // }
+    // }
+}
+
+var mail = ["pvedula1@gitam.in"]; 
+var pass = {
+    "pvedula1@gitam.in": "qwerty123"
+}
+
+function check_log() {
+    var e = document.getElementById("email").value;
+    var f = document.getElementById("password").value;
+    if (mail.includes(e) && pass[e] == f) {
+        var g = document.getElementById("sub");
+        g.style.border = "2px solid black";
+        g.style.borderRadius = "50%";
+        g.innerHTML = "&check;";
+        var alert = document.getElementById("alert");
+        if (alert.style.display == "block") {
+                alert.style.display = "none";
         }
+        document.getElementById("disps").innerText = "Login successful !!!";
+        setTimeout(() => {
+            document.getElementById("login").style.display = "none";
+            document.getElementById("log").style.display = "none";
+            document.getElementById("sign").style.display = "none";
+        }, 1000)
+        document.getElementById("tail").setAttribute("href", "tailfolder.html");
+        document.getElementById("chats").setAttribute("href", "chat.html"); 
     }
+    else {
+        var alert = document.getElementById("alert");
+        alert.style.zIndex = 2;
+        alert.style.display = "block";
+        var span = document.getElementById("cross");
+        span.onclick = function () {
+            alert.style.display = "none";
+        }  
+        setTimeout(() => {
+            alert.style.display = "none";
+        },2000)
+    }
+
+}
+
+function check_sig() {
+    var e = document.getElementById("emails").value;
+    mail.push(e)
+    var f = document.getElementById("passwords").value;
+    var g = document.getElementById("passwordss").value;
+    if (f == g) {
+        pass[e] = f;
+    }
+
+    var g = document.getElementById("subs");
+    g.style.border = "2px solid black";
+    g.style.borderRadius = "50%";
+    g.innerHTML = "&check;";
+    document.getElementById("disp").innerText = "You've signed up as per Gitam database...now Login !!!";
+    setTimeout(() => {
+        document.getElementById("signup").style.display = "none";
+        document.getElementById("sign").style.display = "none";  
+    },1000)
 }
